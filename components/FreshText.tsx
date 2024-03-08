4272df18a20875bddcbbd5e3b7644f5c29a34dfb
Brandoxcode <116689478+Brandoxcode@users.noreply.github.com>
2024-03-08 11:15:09 -0500

    Update FreshText.tsx

--- components/FreshText.tsx
+++ components/FreshText.tsx
@@ -4,7 +4,7 @@ const FreshText = () => {
             <h1 className="bg-cyan-700 text-slate-200 text-5xl p-1.5 flex justify-center items-center">Fresh Fish Delivery</h1>
             <div className=" bg-gray-400 mt-1 text-2xl max-w-[700px] rounded p-1">
                 <ul className="list-disc list-inside">
-                    <li>Order by 4pm for next day delivery.</li>
+                    <li>Order by 1pm for next day delivery.</li>
                     <li>Orders can be picked up at our store in North Captiva during normal business hours (9am-5pm)</li>
                     <li>If your using your phone, please rotate to landscape</li>
                 </ul>
