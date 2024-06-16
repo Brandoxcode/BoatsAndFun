c1dd0ff45cb95b14226cf1fd68d4b4fcb30f8e3d
brandoxcode <brandon.a2001@outlook.es>
2024-06-16 13:54:31 -0400

    test

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
         console.log('body', body)
         const data = await resend.emails.send({
             from: 'Boats and Fun <onboarding@resend.dev>',
-            to: ['brandon.a2001@outlook.es', 'reservations@boatsandfun.com', email],
+            to: ['brandon.a2001@outlook.es', email],
             subject: name + ' ' + 'Seafood Order',
             react: EmailTemplate({ item, name, number, email, memo }),
         });
