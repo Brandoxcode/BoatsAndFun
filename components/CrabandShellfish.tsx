c5b8c59abff5a0925ace49c3fe1906747b7a701a
Brandoxcode <116689478+Brandoxcode@users.noreply.github.com>
2024-06-18 09:34:10 -0400

    Update CrabandShellfish.tsx

--- components/CrabandShellfish.tsx
+++ components/CrabandShellfish.tsx
@@ -1,7 +1,7 @@
 const CrabandShellfish = () => {
     return (
         <div className="flex justify-start items-center border-x-1 border-y-2 border-indigo-100 min-w-[50%]">
-            <p className="p-4 text-4xl text-cyan-950">Craband & Shellfish</p>
+            <p className="p-4 text-4xl text-cyan-950">Crab & Shellfish</p>
         </div>
     )
 }
