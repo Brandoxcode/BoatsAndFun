cccde5a1b62541324e01fecd997fd2b70a59cbe8
Brandon Araya <116689478+Brandoxcode@users.noreply.github.com>
2025-06-21 13:37:46 -0400

    Update route.ts

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
         console.log('body', body)
         const data = await resend.emails.send({
             from: 'Boats and Fun <noreply@boatsandfunorders.com>',
-            to: [email, 'reservations@boatsandfun.com'],
+            to: [email, 'andrewg@boatsandfun.com'],
             subject: name + ' ' + 'Seafood Order',
             react: EmailTemplate({ item, name, number, email, memo }),
         });
